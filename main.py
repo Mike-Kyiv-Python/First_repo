cbd908bd413e869b7cb6c392fe971048c30b4a51
Mike Kyiv <vavazen@gmail.com>
2023-11-01 22:14:32 +0200

    Second day

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
-print("Hello World!")
-2222
+# name = input("Як тебе звати?: ")
+
+# if name:
+#     print(f"Привіт {name}")
+# else:
+#     print("Введи ім'я:")
+
+
+# x = 'apple12345'
+# for i in x:
+#     print (i)
+
+
+
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+# a = 1
+# while True:
+#     print (a)
+#     if a >= 20:
+#         break
+#     a +=1
+
+
+# while True:
+#     text = input("input text: ")
+#     print (text)
+
+#     if text == "exit":
+#         print("! Exit !")
+#         break
+
+
+a = 0
+while a < 6:
+    a +=1
+    if not a % 2:
+        continue
+    print(a)
+
